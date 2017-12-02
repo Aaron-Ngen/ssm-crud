b34b87d6c15d947ac77d888cc96c197fa0acbfb5
Boyce <Aaron.Ngen@outlook.com>
2017-12-03 00:47:37 +0800

    --

---
+++ src/main/java/com/atguigu/crud/controller/DepartmentController.java
@@ -0,0 +1,35 @@
+package com.atguigu.crud.controller;
+
+import java.util.List;
+
+import com.atguigu.crud.service.DepartmentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.atguigu.crud.bean.Department;
+import com.atguigu.crud.bean.Msg;
+
+/**
+ * 处理和部门有关的请求
+ * @author Boyce
+ */
+@Controller
+public class DepartmentController {
+
+	@Autowired
+	private DepartmentService departmentService;
+
+	/**
+	 * 返回所有的部门信息
+	 */
+	@RequestMapping("/depts")
+	@ResponseBody
+	public Msg getDepts(){
+
+		//查出的所有部门信息
+		List<Department> list = departmentService.getDepts();
+		return Msg.success().add("depts",list);
+	}
+}
